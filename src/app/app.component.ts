1a6d96f9af0f0939a84a15e478bde1cf36de7f13
Gyankr <gyankr20@gmail.com>
2020-09-07 14:28:25 +0530

    add all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  FileUploader } from 'ng2-file-upload';
+
+const uploadAPI = 'http://localhost:4000/api/upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng8fileupexample';
+  public uploader: FileUploader = new FileUploader({ url: uploadAPI, itemAlias: 'file' });
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+         console.log('FileUpload:uploaded successfully:', item, status, response);
+         alert('Your file has been uploaded successfully');
+    };
+  }
 }
